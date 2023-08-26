b9f5b5083dbdbdc20cde1422c10b3259f95cea92
Pradeep Rout <pradeeprout56@gmail.com>
2023-08-27 00:05:18 +0530

    <Udemy><Learning Angular>

---
+++ src/app/pkr/pkr.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pkr',
+  templateUrl: './pkr.component.html',
+  // OR
+  //template:'<p>pkr works!</p><pkr-studies>Hey</pkr-studies>',
+  // OR
+  //using backtick multiple direct html code can  be included
+  // template:`<p>pkr works!</p>
+  //           <p pkr-studies>Hey</p>`,
+  styleUrls: ['./pkr.component.css']
+})
+export class PkrComponent {
+
+}
---
+++ src/app/pkrstudies/pkrstudies.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[pkr-studies]',
+  templateUrl: './pkrstudies.component.html'
+})
+export class PkrStudiesComponent {
+}
